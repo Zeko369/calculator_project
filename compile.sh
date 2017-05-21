9e170cfa022100ea418c999a4e0be2459f0c5225
Zeko369 <zekan.fran369@gmail.com>
2017-05-21 16:26:53 +0200

    Added auto compile and started new from scratch

--- compile.sh
+++ compile.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-g++ calculator_mk1_with_brackets.cpp -o test
+filename="./config_files/config_compile.txt"
+
+while read -r line
+do
+	pero="$line"
+done < "$filename"
+
+echo Compiloing
+echo $pero
+
+g++ $pero -o test
